3f4d6e108f14f26e0b0eda36c1a1e8457825b822
Альберт <sabir116rus@gmail.com>
2022-08-22 17:09:21 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 52523;
+        int bonus = ticket_price / 20;
+        System.out.println("У Вас " + bonus + " бонусных милей");
+    }
+}
