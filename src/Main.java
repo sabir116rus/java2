870c6b7ea33802651624f029025d44c0139a535a
Альберт <sabir116rus@gmail.com>
2022-08-26 19:22:46 +0300

    corrected the name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 52523;
-        int bonus = ticket_price / 20;
+        int ticketPrice = 52523;
+        int bonus = ticketPrice / 20;
         System.out.println("У Вас " + bonus + " бонусных милей");
     }
 }
